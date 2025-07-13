666ef615084fb6870ca8758ff51a65f174c2bd0b
Kunal Patil <138603748+kunal0395@users.noreply.github.com>
2025-07-13 17:16:50 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -15,14 +15,5 @@ app.register_blueprint(url_bp)
 app.register_blueprint(note_bp)
 app.register_blueprint(stats_bp)
 
-# Serve React build files
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists("build/" + path):
-        return send_from_directory('build', path)
-    else:
-        return send_from_directory('build', 'index.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
